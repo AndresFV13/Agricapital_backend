937d08d9696cb5a3b697a504b947f7fa91bfed00
Andres Vargas <anmendez845@gmail.com>
2025-04-25 14:16:50 -0500

    resolved problems

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('nest-workshop');
   const options = new DocumentBuilder()
     .setTitle('Nest Workshop')
     .setDescription('Nest Workshop')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('nest-workshop/api', app, document);
+  SwaggerModule.setup('', app, document);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
